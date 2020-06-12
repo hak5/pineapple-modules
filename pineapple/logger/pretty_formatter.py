8e1c0458c3dfe482b49e3011c2cf98dfb6457708
Josh <n3rdcav3@gmail.com>
2020-06-12 19:13:57 -0400

    initial version of pineapple python module (#1)
    * initial version of pineapple python module
    
    * removed useless log message

---
+++ pineapple/logger/pretty_formatter.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class PrettyFormatter(logging.Formatter):
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
