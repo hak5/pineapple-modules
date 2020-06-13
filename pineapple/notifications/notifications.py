412eec6d04b7e613fb2ac6eb51c53239b51b47cc
Marc Egerton <foxtrot@realloc.me>
2020-06-13 21:11:57 +0100

    Module: Move notifications into its own namespace

---
+++ pineapple/notifications/notifications.py
@@ -0,0 +1,41 @@
+import socket
+import json
+
+from pineapple.helpers import Helpers
+
+class Notifications:
+    INFO  = 0
+    WARN  = 1
+    ERROR = 2
+    OTHER = 3
+
+    def send_notification(self, level: int, message: str, module_name: str) -> bool:
+        """
+        Send a notification over the WiFi Pineapples notification socket
+
+        :param level: Notification level
+        :param message: Notification message
+        :return: bool
+        """
+
+        notify_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        notify_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        notify_socket_path = '/tmp/notifications.sock'
+
+        module_notification = {'level': level, 'message': message, 'module_name': module_name}
+        socket_message = Helpers.json_to_bytes(module_notification)
+        status = True
+
+        try:
+            notify_socket.connect(notify_socket_path)
+        except ValueError:
+            return False
+
+        try:
+            notify_socket.sendall(socket_message)
+        except ValueError:
+            status = False
+
+        notify_socket.close()
+
+        return status
