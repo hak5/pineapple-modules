c5d3b2f7e65d65e769ba3c39ba5ee3e74c38580b
Marc <foxtrot@realloc.me>
2020-06-16 22:50:39 +0100

    Add Example Module (#4)
    * example module
    
    * Upgrade example module to Angular 9
    
    * Fix indentation across all files
    
    * Update material module
    
    * Add API as service
    
    * Add material HTML to example module
    
    * Add module icon

---
+++ examplemodule/projects/examplemodule/src/lib/example-module.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { ExampleModuleComponent } from './components/example-module.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import {MaterialModule} from './modules/material/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+const routes: Routes = [
+    { path: '', component: ExampleModuleComponent }
+];
+
+@NgModule({
+    declarations: [ExampleModuleComponent],
+    imports: [
+        RouterModule.forChild(routes),
+        MaterialModule,
+        FlexLayoutModule
+    ],
+    exports: [ExampleModuleComponent]
+})
+export class ExamplemoduleModule { }
