76ebf3884d02b314dfc1a14aca09fad998985ea8
Marc Egerton <foxtrot@realloc.me>
2020-06-16 22:29:43 +0100

    Add API as service

--- examplemodule/projects/examplemodule/src/lib/components/example-module.component.ts
+++ examplemodule/projects/examplemodule/src/lib/components/example-module.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService} from '../services/api.service';
 
 @Component({
     selector: 'lib-example-module',
-    templateUrl: 'example-module.component.html',
-    styleUrls: ['example-module.component.css']
+    templateUrl: './example-module.component.html',
+    styleUrls: ['./example-module.component.css']
 })
 export class ExampleModuleComponent implements OnInit {
-    constructor() { }
+    constructor(private API: ApiService) { }
+
+    testfunc(): void {
+        console.log(this.API);
+    }
 
     ngOnInit() {
+        this.testfunc();
     }
 }
