b8e80499a3621a6640371bedcad65f3bf2a1493c
KoalaV2 <theo.johanson@gmail.com>
2021-05-22 18:22:23 +0200

    Fix regex check for offline and online mac

--- MACInfo/projects/MACInfo/src/module.py
+++ MACInfo/projects/MACInfo/src/module.py
@@ -17,13 +17,14 @@ ONLINE_URL = 'https://macvendors.co/api/'
 @module.handles_action('check_mac_online')
 def check_mac_online(request: Request):
     mac = request.user_input.upper()
-    mac_reg = re.search("^[a-fA-F0-9]{2}([:\-]?[a-fA-F0-9]{2}){2,5}$",mac)
-    if mac_reg:
+    strip_mac = mac.replace(' ','')
+    if '-' in strip_mac:
+        strip_mac = mac.replace('-',':')
+    strip_mac = re.search("^[a-fA-F0-9]{2}([:\-]?[a-fA-F0-9]{2}){2,5}$",strip_mac)
+
+    if strip_mac:
         module.logger.debug("MATCH FOUND")
         module.logger.debug(mac_reg)
-        strip_mac = mac.replace(' ','')
-        if '-' in strip_mac:
-            strip_mac = mac.replace('-',':')
         module.logger.debug(strip_mac)
         response = urllib.request.urlopen(f'{ONLINE_URL}/{strip_mac}/JSON')
         data = response.read()
@@ -47,16 +48,18 @@ def check_mac_online(request: Request):
 def check_mac(request: Request):
     module.logger.debug("Opening file")
     mac = request.user_input.upper()
-    mac_reg = re.search("^[a-fA-F0-9]{2}([:\-]?[a-fA-F0-9]{2}){2,5}$",mac)
-    if mac_reg:
+    nospace_mac = mac.replace(' ','')
+    if ':' in nospace_mac:
+        strip_mac = mac.replace(':','')
+    elif '-' in nospace_mac:
+        strip_mac = mac.replace('-','')
+
+    reg_mac = re.search("^[a-fA-F0-9]{2}([:\-]?[a-fA-F0-9]{2}){2,5}$",nospace_mac)
+
+    if reg_mac:
         with open(OUI_FILE) as f:
             OUIS = json.load(f)
             module.logger.debug("User inputted: " + mac)
-            strip_mac = mac.replace(' ','')
-            if ':' in strip_mac:
-                strip_mac = mac.replace(':','')
-            elif '-' in strip_mac:
-                strip_mac = mac.replace('-','')
             module.logger.debug(strip_mac[:6])
             new_mac = strip_mac[:6]
             company = OUIS.get(new_mac)
