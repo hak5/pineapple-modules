37eb572ccdc2c58693088bbf0e843bd4b017359c
Marc <foxtrot@malloc.me>
2021-07-09 22:46:40 +0100

    Revert "Add SpeedTest v1.1 module (#44)"
    This reverts commit 20752782f77ef4ccb6ed55981e3c23e55d21f793.

--- speedtest/projects/speedtest/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of speedtest
- */
-
-export * from './lib/services/speedtest.service';
-export * from './lib/components/speedtest.component';
-export * from './lib/speedtest.module';
