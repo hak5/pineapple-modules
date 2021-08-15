f85ab5280788dad40042c2e03435f846c46753c5
KoalaV2 <theo.johanson@gmail.com>
2021-08-16 01:15:10 +0200

    Fix quotation

--- mtr/projects/mtr/src/lib/mtr.module.ts
+++ mtr/projects/mtr/src/lib/mtr.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { mtrComponent } from "./components/mtr.component";
-import { RouterModule, Routes } from "@angular/router";
+import { mtrComponent } from './components/mtr.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import { MaterialModule } from "./modules/material/material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { MaterialModule } from './modules/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
-import { LicenseDialogComponent } from "./components/helpers/license-dialog/license-dialog.component";
+import { LicenseDialogComponent } from './components/helpers/license-dialog/license-dialog.component';
 
-const routes: Routes = [{ path: "", component: mtrComponent }];
+const routes: Routes = [{ path: '', component: mtrComponent }];
 
 @NgModule({
     declarations: [mtrComponent, LicenseDialogComponent],
