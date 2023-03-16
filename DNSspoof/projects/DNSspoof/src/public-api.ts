22939f909c8286779c5f425133736099f8f84776
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2023-03-16 09:55:52 -0400

    dnsspoof: Rename DNSspoof for case sensitivity

--- DNSspoof/projects/DNSspoof/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dnsspoof
- */
-
-export * from './lib/services/dnsspoof.service';
-export * from './lib/components/dnsspoof.component';
-export * from './lib/dnsspoof.module';
